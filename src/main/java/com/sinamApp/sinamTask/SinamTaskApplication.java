658702b2d4a60ff530ac1e03878d040013b20356
develop <mahmudov.elshan@gmail.com>
2020-02-19 19:01:17 +0400

    last commit

---
+++ src/main/java/com/sinamApp/sinamTask/SinamTaskApplication.java
@@ -0,0 +1,12 @@
+package com.sinamApp.sinamTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinamTaskApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(SinamTaskApplication.class, args);
+	}
+}
